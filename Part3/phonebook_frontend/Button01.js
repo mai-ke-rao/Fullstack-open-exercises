fcd9f34847bc8c41a5d085b4f8128600ced0c260
Aleksandar Tomic <119106963+mai-ke-rao@users.noreply.github.com>
2024-04-18 07:30:16 +0200

    Delete Part3 directory

--- Part3/phonebook_frontend/Button01.js
+++
@@ -1,7 +0,0 @@
-
-import { toggle, setToggle} from './App'
-const Button01 = ({toggle, func}) =>
-{
-    return(<button onClick={()=>func(!toggle)}>Search</button>)
-}
-export  {Button01};
--- Part3/readme.txt
+++
@@ -1,1 +0,0 @@
-https://pikabu.herokuapp.com/
